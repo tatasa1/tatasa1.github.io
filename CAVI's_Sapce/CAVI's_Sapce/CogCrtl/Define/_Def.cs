a5c157d64622a9b4efa4a5bfef07b1830aaf3346
CAVI <64113704+tatasa1@users.noreply.github.com>
2021-07-12 18:09:45 +0900

    210712
    Let's create a class to use Cognex Vpro

---
+++ CAVI's_Sapce/CAVI's_Sapce/CogCrtl/Define/_Def.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAVI_s_Sapce
+{
+    /// <summary>
+    /// 컨트롤에 사용된 명칭을 정의
+    /// </summary>
+    class _Def
+    {
+        public const int __PROPERTY = 0;
+        public const int __VALUE = 1;
+
+        public const string __YES = "1";
+        public const string __NO = "0";
+        public const string __Low = "Low";
+        public const string __High = "High";
+        public const string __Params = "Params";
+        public const string __ParamsValue = "Value";
+        public const string __ROI = "ROI";
+        public const string __Show = "Show";
+        public const string __Test = "Test";
+        public const string __Run = "Run";
+        public const string __Region = "Region";
+        public const string __Polarity = "Polarity";
+        public const string __All = "All";
+        public const string __CogCircle = "CogCircle";
+        public const string __CogRectangle = "CogRectangle";
+        public const string __CogRectangleAffine = "CogRectangleAffine";
+        public const string __CogPolygon = "CogPolygon";
+
+        public const string __PatternRegion = "PatternRegion";
+        public const string __PatternROI = "PatternROI";
+        public const string __SearchRegion = "SearchRegion";
+        public const string __SearchROI = "SearchROI";
+
+        public const string __Origin = "Origin";
+        public const string __CenterOrigion = "Center Origin";
+        public const string __Train = "Train";
+        public const string __Pattern = "Pattern";
+    }
+
+    public class _DefCaliper
+    {
+        public const string __EdgeMode = "Edge Mode";           //Edge 모드
+        public const string __Edge0Polarity = "Edge0Polarity";  //Edge0 극성
+        public const string __Edge1Polarity = "Edge1Polarity";  //Edge1 극성
+        public const string __ContrastThreshold = "Contrast Threshold";
+        public const string __FilterHalfSizeInPixels = "Filter HalfSize In Pixels";
+        public const string __MaxResults = "Max Results";
+    }
+
+    public class _DefFindLine
+    {
+        public const string __CapliperCount = "Count of Caliper";           //Edge 모드
+        public const string __SearchLength = "Length of Search";           //Edge 모드
+        public const string __ProjectionLength = "Length of Projection";           //Edge 모드
+        public const string __SearchDirection = "Direction of Search";           //Edge 모드
+        public const string __ChangeSearchDirection = "Change Direction of Search";           //Edge 모드
+        public const string __NumToIgnore = "Number to Ignore";           //Edge 모드
+        public const string __DecrementNumToIgnore = "Decrement Number to Ignore";           //Edge 모드
+
+        public const string __EdgeMode = "Edge Mode";           //Edge 모드
+        public const string __Edge0Polarity = "Edge0Polarity";  //Edge0 극성
+        public const string __Edge1Polarity = "Edge1Polarity";  //Edge1 극성
+        public const string __ContrastThreshold = "Contrast Threshold";
+        public const string __FilterHalfSizeInPixels = "Filter HalfSize In Pixels";
+        public const string __MaxResults = "Max Results";
+    }
+
+    public class _DefCalibChecker
+    {
+        public const string __CapliperCount = "Count of Caliper";           //Edge 모드
+        public const string __SearchLength = "Length of Search";           //Edge 모드
+        public const string __ProjectionLength = "Length of Projection";           //Edge 모드
+        public const string __SearchDirection = "Direction of Search";           //Edge 모드
+        public const string __ChangeSearchDirection = "Change Direction of Search";           //Edge 모드
+        public const string __NumToIgnore = "Number to Ignore";           //Edge 모드
+        public const string __DecrementNumToIgnore = "Decrement Number to Ignore";           //Edge 모드
+
+        public const string __EdgeMode = "Edge Mode";           //Edge 모드
+        public const string __Edge0Polarity = "Edge0Polarity";  //Edge0 극성
+        public const string __Edge1Polarity = "Edge1Polarity";  //Edge1 극성
+        public const string __ContrastThreshold = "Contrast Threshold";
+        public const string __FilterHalfSizeInPixels = "Filter HalfSize In Pixels";
+        public const string __MaxResults = "Max Results";
+    }
+}
