fcaae62b27befa181e8b20c99621f6cf988d28fd
CAVI <64113704+tatasa1@users.noreply.github.com>
2021-07-13 12:57:42 +0900

    Enum Study
    make simple code for test with enum

---
+++ CAVI's_Sapce/EnumStudy/Enumstudy.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace EnumStudy
+{
+    partial class Enumstudy
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.pnlMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.Controls.Add(this.btnExit);
+            this.pnlMenu.Controls.Add(this.btnDelete);
+            this.pnlMenu.Controls.Add(this.btnSave);
+            this.pnlMenu.Controls.Add(this.btnOpen);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(744, 88);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnOpen.Location = new System.Drawing.Point(0, 0);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(186, 88);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Tag = "1";
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSave.Location = new System.Drawing.Point(186, 0);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(186, 88);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Tag = "2";
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDelete.Location = new System.Drawing.Point(372, 0);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(186, 88);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Tag = "3";
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnExit.Location = new System.Drawing.Point(558, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(186, 88);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Tag = "4";
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelResult.Location = new System.Drawing.Point(12, 105);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(62, 21);
+            this.labelResult.TabIndex = 1;
+            this.labelResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 157);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.pnlMenu);
+            this.Name = "Form1";
+            this.Text = "Enum study";
+            this.pnlMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ CAVI's_Sapce/EnumStudy/Enumstudy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumStudy
+{
+    public partial class Enumstudy : Form
+    {
+        public enum eMenuChanged : int {_Open =1, _Save, _Delete, _Exit};
+
+        public Enumstudy()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            var _btn = sender as Button;
+            int _tag = Convert.ToInt32(_btn.Tag);
+            this.Cursor = Cursors.WaitCursor;
+            switch ((eMenuChanged)_tag)
+            {
+                case eMenuChanged._Open:
+                    Thread.Sleep(500);
+                    labelResult.Text = "Open이 눌렸습니다!";
+                    break;
+                case eMenuChanged._Save:
+                    Thread.Sleep(500);
+                    labelResult.Text = "Save가 눌렸습니다!";
+                    break;
+                case eMenuChanged._Delete:
+                    Thread.Sleep(500);
+                    labelResult.Text = "Delete가 눌렸습니다!";
+                    break;
+                case eMenuChanged._Exit:
+                    Thread.Sleep(500);
+                    labelResult.Text = "Exit가 눌렸습니다! 안녕히 계세요 여러분";
+                    break;
+            }
+            this.Cursor = Cursors.Default;
+        }
+
+    }
+}
